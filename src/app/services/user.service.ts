cf28d1fb8634134a69c7406f177e306b5c93ca22
Khoa V. Nguyen <khoa.uet58@gmail.com>
2017-03-26 00:14:27 +0700

    all done

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -7,7 +7,7 @@ export class UserService {
   constructor(private http: Http) { }
 
   saveUser(data: any): void {
-    this.http.post('http://evening-wildwood-62950.herokuapp.com/users/save', data).subscribe(response => {
+    this.http.post('/users/save', data).subscribe(response => {
       console.info(response);
     });
   }
